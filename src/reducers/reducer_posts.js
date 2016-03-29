38e413c0bd37d9739e7482fda815c347090d79b8
Nina <nina.liljeblad@gmail.com>
2016-03-30 00:53:53 +0300

    new case for fetching single post

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,5 +1,5 @@
 import { FETCH_POSTS } from '../actions/index';
-
+import {FETCH_SINGLE_POST} from '../actions/index';
 /*state consist of array of blogposts, and object of active post
 blog api doesn't give content to all posts list, just
 when fetching a post with id.. */
@@ -8,6 +8,8 @@ export default function(state = INITIAL_STATE, action){
   switch (action.type) {
     case FETCH_POSTS:
       return { ...state, all:action.payload.data };
+    case FETCH_SINGLE_POST:
+     return {...state, post : action.payload.data};  
     default:
     return state;
   }
